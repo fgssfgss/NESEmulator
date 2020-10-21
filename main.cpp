4157b3704dd1df34381f218e5734ef497a0660d8
Andrew R <andrew612.ru@gmail.com>
2020-10-21 18:16:41 +0300

    Fix uncaught unwind

--- main.cpp
+++ main.cpp
@@ -3,20 +3,14 @@
 #include <emscripten.h>
 #endif 
 
-#ifndef __EMSCRIPTEN__
 int main(int argc, char *argv[]) {
+#ifndef __EMSCRIPTEN__
     if (argc < 2) {
         std::cout << "USAGE: " << argv[0] << " filename.nes" << std::endl;
         return 1;
     }
     Emulator emu(argv[1]);
 #else
-extern "C" {
-    int start_main(void);
-}    
-    
-EMSCRIPTEN_KEEPALIVE
-int start_main(void) {
     Emulator emu("game.nes");
 #endif
     int ret = emu.run();
